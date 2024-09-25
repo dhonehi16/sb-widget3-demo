2d0317b5fc101eb2b5ed61e58dcba6341ccde6e9
dhonehi <dhonehi@gmail.com>
2024-09-25 16:23:36 +0300

    Added editor query param

--- assets/js/editor.js
+++ assets/js/editor.js
@@ -1,30 +1,36 @@
-require.config({ paths: { 'vs': 'https://unpkg.com/monaco-editor@latest/min/vs' }});
+document.addEventListener('DOMContentLoaded', () => {
+    const params = new URLSearchParams(window.location.search)
+    const editorParam = params.get('editor')
 
-// Before loading vs/editor/editor.main, define a global MonacoEnvironment that overwrites
-// the default worker url location (used when creating WebWorkers). The problem here is that
-// HTML5 does not allow cross-domain web workers, so we need to proxy the instantiation of
-// a web worker through a same-domain script
-window.MonacoEnvironment = {
-    getWorkerUrl: function(workerId, label) {
-        return `data:text/javascript;charset=utf-8,${encodeURIComponent(`
+    if (!editorParam || editorParam === 'true') {
+        initEditor()
+    }
+})
+
+function initEditor() {
+    document.head.insertAdjacentHTML('beforeend', '<link href="https://cdn.jsdelivr.net/npm/vscode-codicons@0.0.17/dist/codicon.min.css" rel="stylesheet">')
+    document.body.insertAdjacentHTML('beforeend', '<script type="text/javascript" src="https://unpkg.com/monaco-editor@latest/min/vs/loader.js"></script>')
+
+    require.config({paths: {'vs': 'https://unpkg.com/monaco-editor@latest/min/vs'}});
+
+    window.MonacoEnvironment = {
+        getWorkerUrl: function (workerId, label) {
+            return `data:text/javascript;charset=utf-8,${encodeURIComponent(`
         self.MonacoEnvironment = {
           baseUrl: 'https://unpkg.com/monaco-editor@latest/min/'
         };
         importScripts('https://unpkg.com/monaco-editor@latest/min/vs/base/worker/workerMain.js');`
-        )}`;
-    }
-};
+            )}`;
+        }
+    };
 
-require(["vs/editor/editor.main"], function () {
-    const script = localStorage.getItem('sb-script')
+    require(["vs/editor/editor.main"], function () {
+        const script = localStorage.getItem('sb-script')
 
-    window.editor = monaco.editor.create(document.querySelector('.monaco-editor'), {
-        value: script,
-        language: 'javascript',
-        theme: 'vs-dark',
+        window.editor = monaco.editor.create(document.querySelector('.monaco-editor'), {
+            value: script,
+            language: 'javascript',
+            theme: 'vs-dark',
+        });
     });
-});
-
-setTimeout(() => {
-    console.log(editor.getValue())
-}, 1000)
+}
